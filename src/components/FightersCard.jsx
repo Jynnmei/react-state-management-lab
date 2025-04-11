d3fafa13688d3255746e9e017750bde9f15678e3
Jynnmei <jynn_mei@yahoo.com>
2025-04-11 18:06:12 +0800

    Update FightersCard.jsx

--- src/components/FightersCard.jsx
+++ src/components/FightersCard.jsx
@@ -1,17 +1,55 @@
+// have 3 method can use
+// method 1
 import React from "react";
 import "./Zombie.css";
 
-const FighterCard = ({ fighter, onAddToTeam }) => {
+const FighterCard = (props) => {
   return (
-    <li key={fighter.id} className="fighter-card">
-      <img src={fighter.img} alt={fighter.name} />
-      <h3>{fighter.name}</h3>
-      <p>Price: {fighter.price}</p>
-      <p>Strength: {fighter.strength}</p>
-      <p>Agility: {fighter.agility}</p>
-      <button onClick={() => onAddToTeam(fighter)}>Add</button>
+    <li key={props.fighter.id} className="fighter-card">
+      <img src={props.fighter.img} alt={props.fighter.name} />
+      <h3>{props.fighter.name}</h3>
+      <p>Price: {props.fighter.price}</p>
+      <p>Strength: {props.fighter.strength}</p>
+      <p>Agility: {props.fighter.agility}</p>
+      <button onClick={() => props.onAddToTeam(props.fighter)}>Add</button>
     </li>
   );
 };
 
 export default FighterCard;
+// ================================================================================
+// method 2
+// const FighterCard = (props) => {
+//   const { fighter, onAddToTeam } = props;
+//   return (
+//     <li key={fighter.id} className="fighter-card">
+//       <img src={fighter.img} alt={fighter.name} />
+//       <h3>{fighter.name}</h3>
+//       <p>Price: {fighter.price}</p>
+//       <p>Strength: {fighter.strength}</p>
+//       <p>Agility: {fighter.agility}</p>
+//       <button onClick={() => onAddToTeam(fighter)}>Add</button>
+//     </li>
+//   );
+// };
+
+// export default FighterCard;
+// ================================================================================
+// method 3 - Destructuring
+// import React from "react";
+// import "./Zombie.css";
+
+// const FighterCard = ({ fighter, onAddToTeam }) => {
+//   return (
+//     <li key={fighter.id} className="fighter-card">
+//       <img src={fighter.img} alt={fighter.name} />
+//       <h3>{fighter.name}</h3>
+//       <p>Price: {fighter.price}</p>
+//       <p>Strength: {fighter.strength}</p>
+//       <p>Agility: {fighter.agility}</p>
+//       <button onClick={() => onAddToTeam(fighter)}>Add</button>
+//     </li>
+//   );
+// };
+
+// export default FighterCard;
